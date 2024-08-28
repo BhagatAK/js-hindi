ca6a0ca71c928cfe74e1b81661111eb966f1fa07
Akshay Adhikrao Bhagat <122515168+BhagatAK@users.noreply.github.com>
2024-08-28 03:30:06 +0000

    datatypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,20 @@
+"use strict"
+
+let name = "akshay"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+
+// number = 2 to power 53
+// bigit 
+//string => ""
+// boolean =>true/false
+// null => standalone value
+// undefined =>  
+// symbol => unique
+
+// object
+
+console.log(typeof "akshay");
